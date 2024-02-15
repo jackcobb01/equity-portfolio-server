3a7d8058ffbd07363cc05c3727084251bc2700a1
jackcobb01 <jackrc931@gmail.com>
2024-02-15 00:29:31 -0500

    add connection to mysql server

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # equity-portfolio-server
 Server that the equity portfolio tracker (client) talks to.
+
+Follow the following steps to ensure this server connects to the mysql server:
+1. Create a mysqlConfig.json file in the root of this directory
+2. Create a singular object in that file with the following three field names:
+    - "host"
+    - "user"
+    - "password"
+3. Fill in the values with your appropriate configuration
+
+Here is an example mysql config object:
+{
+    "host": "YOUR_HOST_NAME",
+    "user": "YOUR_USER_NAME",
+    "password": "YOUR_PASSWORD"
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import express from "express";
+import mysql from "mysql";
+import { readFile } from 'fs/promises';
 
 const app = express();
 const port = 4000;
 
+// mysql connection details
+const mysqlConfig = JSON.parse(await readFile(new URL('../mysqlConfig.json', import.meta.url)));
+const connection = mysql.createConnection({
+    host: mysqlConfig.host,
+    user: mysqlConfig.user,
+    password: mysqlConfig.password,
+});
+
+// connect to mysql server
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+});
+
+// get request
 app.get('/', (req, res) => {
     res.send('Welcome to my server!');
 });
 
+// begin listening to queries on specified port
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
